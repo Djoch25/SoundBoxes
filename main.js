3d7dde3560fbbbfcdda9325bf813164ac918365c
Djoch25 <lorenzogioco94@libero.it>
2026-09-06 11:32:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,11 @@ document.body.appendChild(cnv);
 ctx.fillStyle = "#000000";
 
 ctx.fillText("hello world", 250, 50);
+
+let i = 0;
+window.addEventListener("click", () => {
+	ctx.fillText(i++, 250, 150);
+});
 /*
 
 const WIDTH = Math.max(screen.width, screen.height);
